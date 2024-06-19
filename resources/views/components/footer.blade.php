1184d6c7046d5df2a2c54e1abf9e51226e0d38a1
Creerstopher <zagiduulin@vk.com>
2024-06-19 22:29:54 +0300

    update css

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -10,24 +10,24 @@
                     права находятся у создателей шрифтов.
                 </p>
             </div>
-            <div class="footer_items_right">
-                <div class="footer_category">
-                    <h4>Навигация</h4>
-                    <div class="footer_subcategory">
-                        <a href="#">О нас</a>
-                        <a href="?page=catalog">Шрифты</a>
-                        <a href="?page=catalog">Популярное</a>
-                        <!--                        <a href="#">Шрифтовые пары</a>-->
-                    </div>
-                </div>
-                <div class="footer_category">
-                    <h4>Дизайнерам</h4>
-                    <div class="footer_subcategory">
-                        <a href="#">Стать партнером</a>
-                        <a href="#">Условия продажи</a>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="footer_items_right">--}}
+{{--                <div class="footer_category">--}}
+{{--                    <h4>Навигация</h4>--}}
+{{--                    <div class="footer_subcategory">--}}
+{{--                        <a href="#">О нас</a>--}}
+{{--                        <a href="?page=catalog">Шрифты</a>--}}
+{{--                        <a href="?page=catalog">Популярное</a>--}}
+{{--                        <!--                        <a href="#">Шрифтовые пары</a>-->--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="footer_category">--}}
+{{--                    <h4>Дизайнерам</h4>--}}
+{{--                    <div class="footer_subcategory">--}}
+{{--                        <a href="#">Стать партнером</a>--}}
+{{--                        <a href="#">Условия продажи</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 </footer>
